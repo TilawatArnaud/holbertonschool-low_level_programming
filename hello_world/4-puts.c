fb19c59a262e732deb6647a60cf68a2ab95bda0c
TilawatArnaud <tilawatarnaud@gmail.com>
2025-02-14 12:58:32 +0100

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -5,7 +5,10 @@
  * return: 0 no error
  */
 int main(void)
+	/*this starts the fonction main*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	/*this fonction to puts text*/
 			return (0);
+			/* this return 0*/
 }
