aa56c0bdc0963dfcb175c97a7cfe16dfc6e3934e
TilawatArnaud <tilawatarnaud@gmail.com>
2025-02-18 14:23:16 +0100

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,10 @@
 
 void print_alphabet_x10(void)
 	{
-	char c;
 	int dix;
 		for (dix = 1 ; dix > 11; dix++)
 		{
-			for (c = 'a'; c <= 'z'; c++)
-			{
-			_putchar(c);
-			}
+			print_alphabet();
 			_putchar('\n');
 		}
 	}
