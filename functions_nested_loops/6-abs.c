fc7aa87248d96560147b9141ddd7934e20fd4262
TilawatArnaud <tilawatarnaud@gmail.com>
2025-02-19 10:58:41 +0100

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - function that transform a number to it's absolute value
+ *
+ * @n: number to transform
+ *
+ *  Return: Always 0
+ */
+
+int _abs(int n)
+{
+	if (n > 0 || n == 0)
+	{
+	_putchar(n);
+	return (0);
+	}
+	else
+	{
+	n = n * -1;
+	_putchar(n);
+	return (0);
+	}
+}
