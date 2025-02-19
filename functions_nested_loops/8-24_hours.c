6e4fb9d93c93cc4b68937f298776f828476e8969
TilawatArnaud <tilawatarnaud@gmail.com>
2025-02-19 13:26:48 +0100

    8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,7 +8,8 @@
 void jack_bauer(void)
 {
 	int hour, minute;
-	for (hour = 0; hour <24; hour++)
+
+	for (hour = 0; hour < 24; hour++)
 	{
 		for (minute = 0; minute < 60; minute++)
 		{
@@ -17,7 +18,7 @@ void jack_bauer(void)
 			_putchar(':');
 			_putchar((minute / 10) + '0');
 			_putchar((minute % 10) + '0');
-			_putchar('\n');		
+			_putchar('\n');
 		}
 	}
 }
