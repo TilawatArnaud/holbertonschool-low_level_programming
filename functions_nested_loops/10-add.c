bf41c6b36cf2db21e819cdb095a849a277497e29
TilawatArnaud <tilawatarnaud@gmail.com>
2025-02-19 14:02:45 +0100

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - function that adds 2 number together
+ *
+ * @a: first number to add
+ * @b: second number to add
+ *
+ * Return: the result of the addition
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	_putchar(c + '0');
+	_putchar('\n');
+	return (c);
+}
