420debf5f3637f4eb2845f3ec29751d8623e1338
TilawatArnaud <tilawatarnaud@gmail.com>
2025-02-19 14:40:23 +0100

    11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,22 +8,21 @@
 
 void print_to_98(int n)
 {
-	int i;
-	if(n < 98)
+	if (n < 98)
 	{
-		for(i = n; i < 98; i++)
+		for (;n <= 97; n++)
 		{
-			printf("%d, ",i);
-			printf("98\n");
+			printf("%d, ", n);
 		}
+		printf ("98\n");
 	}
 	else if (n > 98)
 	{
-		for(i = n; i > 98; i++)
+		for (;n >= 99; n--)
 		{
-			printf("%d, ",i);
-			printf("98\n");
+			printf("%d, ", n);
 		}
+		printf("98\n");
 	}
 	else
 	printf("98\n");
