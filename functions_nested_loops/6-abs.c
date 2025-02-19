7c6833be5be36cc7cbb099125c0ec3aff20cad9e
TilawatArnaud <tilawatarnaud@gmail.com>
2025-02-19 11:26:38 +0100

    6-abs.c

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -10,17 +10,9 @@
 
 int _abs(int n)
 {
-	if (n > 0 || n == 0)
+	if (n < 0)
 	{
-	_putchar(n);
-	_putchar('\n');
-	return (0);
+		return (-n);
 	}
-	else
-	{
-	n = (n) * (-1);
-	_putchar(n);
-	_putchar('\n');
-	return (0);
-	}
+	return (n);
 }
