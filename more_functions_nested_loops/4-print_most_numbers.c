0b3582a5aa2f98ed0fe33982f467c6f74c740163
TilawatArnaud <tilawatarnaud@gmail.com>
2025-02-24 11:17:56 +0100

    4-print_most_numbers.c

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print the number
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	i = '0';
+	if (i >= '0' && i <= '9')
+	{
+		i++;
+	}
+	else if (i != '2' && i != '4')
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
