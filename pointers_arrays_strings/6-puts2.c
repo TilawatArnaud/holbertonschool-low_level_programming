227f20874fb95bba45ab6dce2bb35a9bda7adef4
TilawatArnaud <tilawatarnaud@gmail.com>
2025-02-26 12:22:10 +0100

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str: string
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str + 2;
+	}
+	_putchar('\n');
+}
