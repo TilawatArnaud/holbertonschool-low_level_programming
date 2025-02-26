60f745677a5f822f865c3d3c4da72fc67a1ca7b9
TilawatArnaud <tilawatarnaud@gmail.com>
2025-02-26 13:43:11 +0100

    6-puts2.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int i, len = 0;
+	int len;
+
+	i = 0;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+		if (len = *str / 2)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
