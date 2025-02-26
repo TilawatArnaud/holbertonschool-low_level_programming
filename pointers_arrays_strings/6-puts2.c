2ee29607f63fbca27ef58487fc929e1b874182df
TilawatArnaud <tilawatarnaud@gmail.com>
2025-02-26 12:35:18 +0100

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,15 +6,10 @@
 
 void puts2(char *str)
 {
-	int i = 0;
-
-	while (str[i] != '\0')
+	while (*str != '\0')
 	{
-		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
-		i++;
+		_putchar(*str);
+		str += 2;
 	}
 	_putchar('\n');
 }
