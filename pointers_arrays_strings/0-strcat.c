dadfbc839fb4e12dbb85442917e0a9deeb66f24c
TilawatArnaud <tilawatarnaud@gmail.com>
2025-02-27 13:10:09 +0100

    0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,10 +9,10 @@
 char *_strcat(char *dest, char *src)
 {
 	int i = 0;
-
-	while (dest[i] != '\0)')
-		i++;
 	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
