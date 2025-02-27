3f15f9f8aa00af71d6b8daabe1ccf6433471680d
TilawatArnaud <tilawatarnaud@gmail.com>
2025-02-27 15:05:45 +0100

    4-rev_array.c

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -9,17 +9,25 @@
 void reverse_array(int *a, int n)
 {
 	int i;
+	int temp;
 
-	i = n;
-	while (n <= i)
+	// Reverse the array in place
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	}
+
+	// Print the reversed array
+	for (i = 0; i < n; i++)
 	{
 		if (i != 0)
 		{
-			_putchar(',');
+			_putchar(','); // Print comma for separation
 			_putchar(' ');
 		}
-		_putchar(a[i]);
-		i--;
+		_putchar(a[i] + '0'); // Convert integer to character for printing
 	}
 	_putchar('\n');
 }
