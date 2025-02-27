1fe3471fc5c05d602fa2f4d72cce460ccdb34192
TilawatArnaud <tilawatarnaud@gmail.com>
2025-02-27 13:36:28 +0100

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copies the string pointed to by src, including the
+ *           terminating null byte (\0), to the buffer pointed to by dest.
+ * 			 limited by n bytes
+ * @dest: Destination buffer.
+ * @src: Source string.
+ * @n: bytes limit
+ * Return: Pointer to dest.
+ */
+
+char *_strcpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
