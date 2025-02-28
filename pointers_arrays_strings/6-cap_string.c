439b300b05c784dd8f35a5a6325bb3611dd9dc87
root <root@DESKTOP-U3PAFE4.>
2025-02-28 11:29:59 +0100

    6-cap_string.c

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * cap_string - function that capitalizes all words of a string
+ * @s: string
+ * Return: String capitalized
+ */
+
+char *cap_string(char *s)
+{
+    int i;
+
+    if(s[0] >= 97 && s[0] <= 122)
+        {
+        s[0] -= 32;
+        }
+    for(i=1; s[i] != '\0'; i++)
+        {
+            if(s[i-1] == 32)
+            {
+                if(s[i] >= 97 && s[i] <= 122)
+                {
+                s[i] -= 32;
+                }
+            }
+    }
+}
