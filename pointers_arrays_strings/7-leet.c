eb31a8fc09a32cd44cc94ef1cb29ecd34d07abbd
TilawatArnaud <tilawatarnaud@gmail.com>
2025-02-28 14:10:03 +0100

    7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -6,7 +6,7 @@
 char *leet(char *s)
 {
 	int i;/*parcoureur de tableau */
-	int j;/*boucler tableau*/
+	int j = 0;/*boucler tableau*/
 	char min[] = {'a','e', 'o', 't', 'l'};
 	char maj[] = {'A', 'E', 'O', 'T', 'L'};
 	char num[] = {4, 3, 0, 7, 1};
@@ -14,9 +14,10 @@ char *leet(char *s)
 	while(s[i] != '\0')
 	{
 	i++;
-	if(*s == min[i] || *s == maj[i])
+	if(*s == min[j] || *s == maj[j])
 		{
-			*s = num[i];
+			*s = num[j];
+			j++;
 		}
 	}
 	return (s);
