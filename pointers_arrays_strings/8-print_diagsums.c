8faa24694613b8a77bf8cba84f1321c00eb87247
TilawatArnaud <tilawatarnaud@gmail.com>
2025-03-04 12:23:17 +0100

    8-print_diagsums.c

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_diagsums(int *a, int size)
+{
+    int i;
+    int primary_diag_sum = 0;
+    int secondary_diag_sum = 0;
+
+    for (i = 0; i < size; i++)
+    {
+        primary_diag_sum += *(a + i * size + i);
+        secondary_diag_sum += *(a + i * size + (size - i - 1));
+    }
+
+    printf("%d, %d\n", primary_diag_sum, secondary_diag_sum);
+}
