50734244ba5ef84416bf5bf10e8e03cc04071cc4
TilawatArnaud <tilawatarnaud@gmail.com>
2025-03-04 12:05:17 +0100

    7-print_chessboard.c

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * print_chessboard - Prints an 8x8 chessboard.
- * @a: Pointer to an 8x8 array of characters.
+ * print_chessboard - Prints an 8x8 chessboard
+ * @a: Pointer to an 8x8 array of characters
  */
 void print_chessboard(char (*a)[8])
 {
     int i, j;
 
-    for (i = 0; i < 8; i++) // Parcourir les lignes
+    for (i = 0; i < 8; i++)
     {
-        for (j = 0; j < 8; j++) // Parcourir les colonnes
+        for (j = 0; j < 8; j++)
         {
-            _putchar(a[i][j]); // Afficher chaque caractère
+            _putchar(a[i][j]);
         }
-        _putchar('\n'); // Nouvelle ligne après chaque ligne de l'échiquier
+        _putchar('\n');
     }
 }
