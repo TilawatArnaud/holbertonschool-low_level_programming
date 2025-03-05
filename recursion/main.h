8e29d55431fea154db5a0b3de13057b9b8a9e13c
TilawatArnaud <tilawatarnaud@gmail.com>
2025-03-05 11:51:29 +0100

    main.h

---
+++ recursion/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * file: main.h
+ * Auth: TILAWAT Arnaud
+ * Desc: All functions used for project recursion
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif
