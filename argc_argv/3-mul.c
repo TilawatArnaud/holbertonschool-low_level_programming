e9a1858c6b35d78759b167e7d78d7225944d1331
TilawatArnaud <tilawatarnaud@gmail.com>
2025-03-06 15:48:54 +0100

    3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,17 +12,17 @@ int main(int argc, char *argv[])
 {
 	int a, b;
 
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-
-	if (argc == 3)
-	{
-		printf("%d\n", a * b);
-	}
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	return (0);
+	else
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+
+		printf("%d\n", a * b);
+		return (0);
+	}
 }
