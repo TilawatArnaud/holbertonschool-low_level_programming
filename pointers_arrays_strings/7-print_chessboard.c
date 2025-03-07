cd48aa3e4966c4b0532dd07c5732258ea2c613ce
TilawatArnaud <tilawatarnaud@gmail.com>
2025-03-07 12:14:09 +0100

    7-print_chessboard.c

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -5,19 +5,19 @@
  */
 void print_chessboard(char (*a)[8])
 {
-    int i;
-    int j;
+	int i;
+	int j;
 
-    i = j = 0;
-    while (i < 8)
-    {
-        j = 0;
-        while (j < 8)
-        {
-            _putchar(a[i][j]);
-            j++;
-        }
-        _putchar('\n');
-        i++;
-    }
+	i = j = 0;
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
