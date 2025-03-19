dbc9beed6ef1ef2361990e1991dff0c8f5339e76
TilawatArnaud <tilawatarnaud@gmail.com>
2025-03-19 11:41:21 +0100

    3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,44 +3,45 @@
 #include <stdio.h>
 
 /**
- * print_all - Prints all arguments based on the format specifiers.
+ * print_all - Prints anything based on format specifiers.
  * @format: A string containing the format specifiers.
  */
-
 void print_all(const char * const format, ...)
 {
 	va_list args;
 	unsigned int i = 0;
-	char *str;
-	char c;
-	int num;
-	float f;
-	int printed = 0;
+	char *str, *sep = "";
 
 	va_start(args, format);
+
 	while (format && format[i])
 	{
-		if (printed)
-			printf(", ");
-		if (format[i] == 'c')
-			printf("%c", va_arg(args, int));
-		if (format[i] == 'i')
-			printf("%d", va_arg(args, int));
-		if (format[i] == 'f')
-			printf("%f", va_arg(args, double));
-		if (format[i] == 's')
+		switch (format[i])
 		{
-			str = va_arg(args, char *);
-			if (str)
-				printf("%s", str);
-			if (!str)
-				printf("(nil)");
+			case 'c':
+				printf("%s%c", sep, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", sep, va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s%s", sep, str);
+				break;
+			default:
+				i++;
+				continue;
 		}
-		if (format[i] == 'c' || format[i] == 'i' ||
-		 format[i] == 'f' || format[i] == 's')
-			printed = 1;
+
+		sep = ", ";
 		i++;
 	}
+
 	printf("\n");
 	va_end(args);
 }
