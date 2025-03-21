969dfa5eced64175d9719aebd2ef85a6183b1fd9
TilawatArnaud <tilawatarnaud@gmail.com>
2025-03-21 13:33:37 +0100

    3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,4 +1,7 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>  // for isdigit
 
 /**
  * main - Performs simple operations
@@ -8,33 +11,39 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
-	int (*op_func)(int, int);
+    int num1, num2;
+    int (*op_func)(int, int);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
+    if (!isdigit(argv[1][0]) || !isdigit(argv[3][0]))
+    {
+        printf("Error\n");
+        exit(98);
+    }
 
-	op_func = get_op_func(argv[2]);
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
 
-	if (op_func == NULL || argv[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
+    op_func = get_op_func(argv[2]);
 
-	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
+    if (op_func == NULL)
+    {
+        printf("Error\n");
+        exit(99);
+    }
 
-	printf("%d\n", op_func(num1, num2));
+    if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0) && num2 == 0)
+    {
+        printf("Error\n");
+        exit(100);
+    }
 
-	return (0);
+    printf("%d\n", op_func(num1, num2));
+
+    return (0);
 }
