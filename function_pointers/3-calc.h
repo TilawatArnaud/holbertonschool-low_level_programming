4479de37d0affd5b2d8349164700aef1c16cd149
TilawatArnaud <tilawatarnaud@gmail.com>
2025-03-21 13:28:05 +0100

    3

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,6 +1,7 @@
 #ifndef CALC_H
 #define CALC_H
 
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -15,12 +16,18 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
+/* Function prototypes */
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 
+/**
+ * get_op_func - Selects the correct function for the operation
+ * @s: The operator passed as argument
+ * Return: Pointer to the corresponding function, or NULL if invalid
+ */
 int (*get_op_func(char *s))(int, int);
 
 #endif
