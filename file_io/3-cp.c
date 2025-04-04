d8d1a0fd2ced2c308f1dd668630b2826cb3cdb29
TilawatArnaud <tilawatarnaud@gmail.com>
2025-04-04 13:06:56 +0200

    3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -32,11 +32,9 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 		print_error(97, "Usage: cp file_from file_to\n", "");
-
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
 		print_error(98, "Error: Can't read from file %s\n", av[1]);
-
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
@@ -59,12 +57,14 @@ int main(int ac, char **av)
 		close(fd_to);
 		print_error(98, "Error: Can't read from file %s\n", av[1]);
 	}
-
 	if (close(fd_from) == -1)
-		print_error(100, "Error: Can't close fd %d\n", av[1]);
-
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
 	if (close(fd_to) == -1)
-		print_error(100, "Error: Can't close fd %d\n", av[2]);
-
-	return (0);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
 }
